f5fd0df87404c0a4e52c056b249ed5b26bda9edd
Sueyoshi-Ayako <sueyoshi-ayako@aeonpeople.biz>
2025-03-21 09:07:25 +0900

    Add files via upload

--- js/url_seni_movie.js
+++ js/url_seni_movie.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             "./signage03.html",
 
-            "./signage07_benkyokai.html",
+            // "./signage07_benkyokai.html",
 
             "./signage_youtube.html",
 			
