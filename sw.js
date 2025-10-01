8693f8429b8c6289dd0e43f41130acc0b1bd16a6
須釜愛 <aisugama@gmail.com>
2025-10-01 16:41:03 +0900

    PWA修正

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
-self.addEventListener('fetch', function(e) {
-})
+self.addEventListener("install", (event) => {
+    console.log("Service Worker: Installed");
+});
+
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker: Activated");
+});
+
+// ネットワークから取得（キャッシュ処理はなし）
+self.addEventListener("fetch", (event) => {
+    event.respondWith(fetch(event.request));
+});
