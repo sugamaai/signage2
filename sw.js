91349d25e0a08218e44ecea9d44ff5c0b78d8860
須釜愛 <aisugama@gmail.com>
2025-10-03 10:01:55 +0900

    PWA修正

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function (event) {
     console.log("SW installed");
+    self.skipWaiting(); // すぐに有効化
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function (event) {
     console.log("SW activated");
+    event.waitUntil(self.clients.claim()); // 全ページに即反映
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            // ネットワークエラー時に落ちないようにする
+            return Response.error();
+        })
+    );
 });
