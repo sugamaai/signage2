bb7276ea2a6649547c15958b1f57d2fa7c5f7479
須釜愛 <aisugama@gmail.com>
2025-10-07 09:52:52 +0900

    sw.js修正

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
-self.addEventListener("install", function (event) {
-    console.log("SW installed");
-    event.waitUntil(
-        caches.open("v1").then(function (cache) {
-            return cache.addAll(["/offline.html", "/index.html"]);
-        })
-    );
-});
+// self.addEventListener("install", function (event) {
+//     console.log("SW installed");
+//     event.waitUntil(
+//         caches.open("v1").then(function (cache) {
+//             return cache.addAll(["/offline.html", "/index.html"]);
+//         })
+//     );
+// });
 
-self.addEventListener("activate", function (event) {
-    console.log("SW activated");
-    event.waitUntil(self.clients.claim());
-});
+// self.addEventListener("activate", function (event) {
+//     console.log("SW activated");
+//     event.waitUntil(self.clients.claim());
+// });
 
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function () {
-            return caches.open("v1").then(function (cache) {
-                return cache.match(event.request).then(function (response) {
-                    return response || cache.match("/offline.html");
-                });
-            });
-        })
-    );
-});
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         fetch(event.request).catch(function () {
+//             return caches.open("v1").then(function (cache) {
+//                 return cache.match(event.request).then(function (response) {
+//                     return response || cache.match("/offline.html");
+//                 });
+//             });
+//         })
+//     );
+// });
+self.addEventListener('fetch', function (e) {
+})
